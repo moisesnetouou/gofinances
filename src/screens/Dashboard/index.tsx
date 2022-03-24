1cde213dafaf5c24b12b810affbdcbcd21eac3cf
Moisés <moisesnetored@gmail.com>
2022-03-24 10:04:44 -0400

    feat: Conceptualized and resolved Pixel Density, Header in process of creation.

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import {Container, Title} from './styles';
+import { Text } from 'react-native';
+import {
+  Container, 
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName
+} 
+from './styles';
 
 export function Dashboard(){
   return(
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        
+        <UserWrapper>
+          <UserInfo>
+            <Photo 
+              source={{uri: 'https://avatars.githubusercontent.com/u/39030702?v=4'}} 
+            />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Moisés</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+
+      </Header>
     </Container>
   );
 }
