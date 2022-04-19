f12af00f9a55ccfd96acca3983e6c6615d379a68
Moisés <moisesnetored@gmail.com>
2022-04-19 11:41:39 -0400

    feat: Registration and Category/Modal Screens created.

--- App.tsx
+++ App.tsx
@@ -9,7 +9,9 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
-import { Dashboard } from './src/screens/Dashboard';
+// import { Dashboard } from './src/screens/Dashboard';
+import { Register } from './src/screens/Register';
+
 import AppLoading from 'expo-app-loading';
 
 export default function App() {
@@ -23,7 +25,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   )
 }
---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,84 @@
+import React, {useState} from 'react';
+import { Modal } from 'react-native';
+import { Input } from '../../components/Forms/Input';
+import { Button } from '../../components/Forms/Button';
+
+import {
+  Container,
+  Header,
+  Title,
+  Form,
+  Fields,
+  TransactionsTypes
+} from './styles';
+import { TransactionTypeButton } from '../../components/Forms/TransactionTypeButton';
+import { CategorySelectButton } from '../../components/Forms/CategorySelectButton';
+
+import {CategorySelect} from '../CategorySelect'
+
+export function Register(){
+  const [transactionType, setTransactionType] = useState('');
+  const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+  const [category, setCategory] = useState({
+    key: 'category',
+    name: "Categoria",
+  });
+
+  function handleTransactionTypeSelect(type: 'up' | 'down'){
+    setTransactionType(type)
+  }
+
+  function handleOpenSelectCategoryModal(){
+    setCategoryModalOpen(true);
+  }
+
+  function handleCloseSelectCategoryModal(){
+    setCategoryModalOpen(false);
+  }
+
+  return(
+    <Container>
+      <Header>
+        <Title>Cadastro</Title>
+      </Header>
+
+      <Form>
+        <Fields>
+          <Input placeholder="Nome" />
+          <Input placeholder="Preço" />
+
+          <TransactionsTypes>
+            <TransactionTypeButton 
+              type="up"
+              title="Income"
+              onPress={()=> handleTransactionTypeSelect('up')}
+              isActive={transactionType === 'up'}
+            />
+
+            <TransactionTypeButton 
+              type="down"
+              title="Outcome"
+              onPress={()=> handleTransactionTypeSelect('down')}
+              isActive={transactionType === 'down'}
+            />
+          </TransactionsTypes>
+
+          <CategorySelectButton 
+            title={category.name}
+            onPress={handleOpenSelectCategoryModal}
+          />
+        </Fields>
+
+        <Button title="Enviar" />
+      </Form>
+
+      <Modal visible={categoryModalOpen}>
+        <CategorySelect 
+          category={category}
+          setCategory={setCategory}
+          closeSelectCategory={handleCloseSelectCategoryModal}
+        />
+      </Modal>
+    </Container>
+  );
+}
