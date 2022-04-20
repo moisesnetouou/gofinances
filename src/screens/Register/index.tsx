f98b153aa40c6ef730cb93a262fd13d14adb8905
Moisés <moisesnetored@gmail.com>
2022-04-20 11:14:55 -0400

    feat: Data saving adjustments.

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,5 +1,4 @@
-import React, {useState, useEffect} from 'react';
-import {useForm} from 'react-hook-form';
+import React, {useState} from 'react';
 import { 
   Keyboard, 
   Modal, 
@@ -7,6 +6,9 @@ import {
 } from 'react-native';
 import * as Yup from 'yup';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import uuid from 'react-native-uuid';
+import {useNavigation} from '@react-navigation/native';
+import {useForm} from 'react-hook-form';
 
 import {yupResolver} from '@hookform/resolvers/yup';
 import {TouchableWithoutFeedback} from 'react-native-gesture-handler'
@@ -48,6 +50,7 @@ export function Register(){
   const {
     control,
     handleSubmit,
+    reset,
     formState: {errors}
   } = useForm<FormData>({
     resolver: yupResolver(schema)
@@ -57,6 +60,8 @@ export function Register(){
     key: 'category',
     name: "Categoria",
   });
+
+  const navigation = useNavigation();
 
   function handleTransactionTypeSelect(type: 'up' | 'down'){
     setTransactionType(type)
@@ -80,10 +85,12 @@ export function Register(){
     }
 
     const newTransaction = {
+      id: String(uuid.v4()),
       name: form.name,
       amount: form.amount,
       transactionType,
-      category: category.key
+      category: category.key,
+      date: new Date()
     }
 
     try {
@@ -97,25 +104,22 @@ export function Register(){
       
 
       await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted))
+
+      reset();
+      setTransactionType('');
+      setCategory({
+        key: 'category',
+        name: 'Categoria'
+      });
+
+      //@ts-ignore
+      navigation.navigate('Listagem')
+
     } catch (error) {
       console.log(error);
       Alert.alert("Não foi possível salvar")
     }
   }
-
-  useEffect(()=> {
-    async function loadData(){
-      const data = await AsyncStorage.getItem(dataKey);
-
-      console.log(JSON.parse(data!));
-    }
-
-    loadData();
-    // async function removeAll(){
-    //   await AsyncStorage.removeItem(dataKey);
-    // }
-    // removeAll();
-  }, [])
 
   return(
     // fecha o teclado em qualquer area
