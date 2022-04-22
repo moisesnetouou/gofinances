b039e214c3a66d0f3030aa9fc9d978b0047c00e0
Moisés <moisesnetored@gmail.com>
2022-04-22 10:43:34 -0400

    feat: SignIn screen interface and SVG typing correction.

--- App.tsx
+++ App.tsx
@@ -14,10 +14,11 @@ import {
 
 import theme from './src/global/styles/theme';
 
-import {AppRoutes} from './src/routes/app.routes';
+// import {AppRoutes} from './src/routes/app.routes';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { StatusBar } from 'react-native';
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
   const [fonstLoaded] = useFonts({
@@ -33,7 +34,8 @@ export default function App() {
       <ThemeProvider theme={theme}>
         <StatusBar barStyle="light-content" backgroundColor="#5636D3" />
         <NavigationContainer>
-          <AppRoutes />
+          {/* <AppRoutes /> */}
+          <SignIn />
         </NavigationContainer>
       </ThemeProvider>
     </GestureHandlerRootView>
---
+++ src/@types/svg/index.d.ts
@@ -0,0 +1,6 @@
+declare module "*.svg" {
+  import React from 'react';
+  import {} from 'react-native';
+  const content: React.FC<SvgProps>
+  export default content;
+}
